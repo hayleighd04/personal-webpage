2925760b378095cda51e6895d17c43fe10ec0710
hayleighd04 <hayleigh.elizabeth04@gmail.com>
2026-03-28 00:54:18 -0400

    Updates

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css'
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p>Built by <span className={styles.name}>Hayleigh Daughtrey</span> · {new Date().getFullYear()}</p>
-    </footer>
-  )
-}
